f8803b90d6c0fd968fa229cb700671546ab49392
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-20 21:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+Car.prototype.getmakemodel=function(){
+return '${this.make} ${this.model}';
+		
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make.model);
+	this.topSpeed=topSpeed;
+}
+SportsCar.prototype=object.create(car.prototype);
+SportsCar.prototype.constructor=SportsCar;
+SportsCar.protoype.gettopSpeed=function(){
+	return this.topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
